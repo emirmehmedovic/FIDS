b022359f02d31a047a0037fe1eadcfb7ed5bf239
emirmehmedovic <emir.m@live.com>
2025-03-26 12:15:08 +0100

    fix: Use ReactDOM.render for WebOS 4.x compatibility

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -4,18 +4,15 @@ import './polyfills';
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'; // Bootstrap prvi
 import './index.css'; 
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom'; // Use 'react-dom' instead of 'react-dom/client'
 import App from './App';
 import { Provider } from 'react-redux';
 import store from './store/store'; // Redux store
 
 
-
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-root.render(
+ReactDOM.render(
   <Provider store={store}>
     <App />
-  </Provider>
+  </Provider>,
+  document.getElementById('root')
 );
